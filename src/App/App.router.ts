dee0955f9cfebd027dc2bed4d494085094ce1a33
Ignacio Mazzara <nachomazzara@gmail.com>
2021-11-25 14:58:23 -0300

    feat: comment

--- src/App/App.router.ts
+++ src/App/App.router.ts
@@ -1,7 +1,6 @@
 import { Router } from 'express'
 
 export const useAppRouter = (router: Router) =>
-  // App page route
   router.get('/info', function (_, res) {
     res.setHeader('Content-Type', 'application/json')
     res.send({
