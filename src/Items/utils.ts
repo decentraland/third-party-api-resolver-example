9d4782fda1416b1789a4b0eeab72a9f1a74d22bf
Gabriel Diaz <gabrieldiaz31@gmail.com>
2024-04-26 18:42:21 -0300

    feat: Add new TP collection in amoy

--- src/Items/utils.ts
+++ src/Items/utils.ts
@@ -1,5 +1,5 @@
 export function buildURN(registryId: string, itemId: string): string {
   // The URN is built using the default prefix and using the registry and item id.
   // More info in how to generate a valid URN here: https://github.com/decentraland/adr/blob/main/docs/ADR-42-third-party-assets-integration.md#third-party-item-urn
-  return `urn:decentraland:mumbai:collections-thirdparty:${registryId}:${itemId}`
+  return `urn:decentraland:amoy:collections-thirdparty:${registryId}:${itemId}`
 }
